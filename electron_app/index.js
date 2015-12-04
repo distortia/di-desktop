361fe34ec9f701890941a3e7a947f86a8ed18dbb
Nick <nickstalter@gmail.com>
2015-12-03 21:10:39 -0500

    Got express, handlebars, sass all bundled and working.

---
+++ electron_app/index.js
@@ -0,0 +1,27 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+var mainWindow = null;
+
+app.on('window-all-closed', function () {
+    if (process.platform != 'darwin')
+        app.quit();
+});
+
+app.on('ready', function () {
+    mainWindow = new BrowserWindow({
+        "auto-hide-menu-bar": true,
+        "web-preferences": {
+            "node-integration": false
+        },
+        width: 600,
+        height: 400
+    });
+
+    mainWindow.loadURL('http://127.0.0.1:3000/');
+    //mainWindow.openDevTools();
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+});
